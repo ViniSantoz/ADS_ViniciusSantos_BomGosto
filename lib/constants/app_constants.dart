bdf93d1322464ec79499c773adb73e6d28443dfb
SantoVini <155762425+ViniSantoz@users.noreply.github.com>
2026-05-27 16:55:30 -0300

    revisao

--- lib/constants/app_constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  // Usando a API gratuita AbstractAPI (https://app.abstractapi.com/api/email-validation)
-  // Crie uma conta gratuita e cole sua chave aqui
-  static const String emailApiKey = '4605e3f8d59141acaa0694ad1d9831c5';
-  static const String emailApiBaseUrl =
-      'https://emailvalidation.abstractapi.com/v1/';
-}
